0e921fab8f17e0f284a9a4afb64c1ea749ee5a91
Ighor Gomes <ighorgoncalves@gmail.com>
2023-10-22 11:31:12 -0300

    Update ConfirmarDados.php

--- ConfirmarDados.php
+++ ConfirmarDados.php
@@ -9,7 +9,7 @@ ob_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="confirmardados.css">
+    <link rel="stylesheet" href="ConfirmarDados.css">
     <link rel="shortcut icon" href="media/icons/navIcon.png" type="image/x-icon" />
 
     <title>Dados Enviados</title>
